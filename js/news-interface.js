458d3a96223f4ceabbf8ae7819b6cae109c09a48
Ben Williams and Monique St. Laurent <ben+monique@>
2017-05-24 14:39:16 -0700

    add images and links in new tabs

--- js/news-interface.js
+++ js/news-interface.js
@@ -2,7 +2,7 @@ var News = require('./../js/news.js').newsModule;
 var displayNews = function(sources) {
   $('#showNews').empty();
   sources.forEach(function(source) {
-    $('#showNews').append(`<li id="news-value"> <strong><a href="${source.url}">  ${source.title}  </a></strong><br>  ${source.description}<br><hr></li>`);
+    $('#showNews').append(`<img src="${source.urlToImage}"/><li id="news-value"> <strong><a href="${source.url}" target="_blank">  ${source.title}  </a></strong><br>  ${source.description}</li><br><hr>`);
   });
 };
 
